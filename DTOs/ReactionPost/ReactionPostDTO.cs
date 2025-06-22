7d33a3258c1c1f65b65bef87c9d59b7fd7b32756
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-06-22 17:20:35 -0300

    Add ReactionPostDTO

---
+++ DTOs/ReactionPost/ReactionPostDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.entities.enums;
+
+namespace blog.DTOs.ReactionPost
+{
+    public class ReactionPostDTO
+    {
+        [Required] public long PostId { get; set; }
+        [Required] public LikeOrDislike action { get; set; }
+    }
+}
