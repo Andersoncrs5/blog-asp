2d7cedb8ceeacac09530490cbd21ff0cc0d70e3f
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-01 19:23:17 -0300

    change type in method get  and new method exists

--- SetRepositories/IRepositories/IUserConfigRepository.cs
+++ SetRepositories/IRepositories/IUserConfigRepository.cs
@@ -10,11 +10,11 @@ namespace blog.SetRepositories.IRepositories
 {
     public interface IUserConfigRepository
     {
-        Task<UserConfigEntity> GetAsync(ApplicationUser user, bool includeRelations = false);
+        Task<UserConfigEntity?> GetAsync(ApplicationUser user);
         Task DeleteAsync(UserConfigEntity config);
+        Task<bool> Exists(string userId);
         Task<UserConfigEntity> CreateAsync(CreateUserConfigDTO dto ,ApplicationUser user);
-        Task<UserConfigEntity> UpdateAsync(UpdateUserConfigDTO dto ,ApplicationUser user);
-
+        Task<UserConfigEntity> UpdateAsync(UpdateUserConfigDTO dto ,ApplicationUser user, UserConfigEntity config);
 
     }
 }
